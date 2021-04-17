92980b88e51e82cdff08e5e0f8bd3d5c84954c2e
Michael Pobega <pobega@gmail.com>
2021-04-17 17:08:14 -0400

    flycast: initial support
    Setup the writeable ROMs directory for the sandbox and initialize a
    default writeable emu.cfg (using a blank file, since Flycast.exe seems
    to automatically fill the file with sane defaults)

--- scripts/fightcade-launcher.sh
+++ scripts/fightcade-launcher.sh
@@ -23,8 +23,9 @@ touch ${DATADIR}/logs/fcade.log.3
 mkdir -p ${DATADIR}/ROMs/fbneo
 mkdir -p ${DATADIR}/ROMs/ggpofba
 mkdir -p ${DATADIR}/ROMs/snes9x
+mkdir -p ${DATADIR}/ROMs/flycast
 
-# Emulator writable directories
+# Emulator writable files & directories
 # FBNeo
 mkdir -p ${DATADIR}/config/fcadefbneo
 cp -n /app/fightcade/Fightcade/emulator/fbneo/config/fcadefbneo.default.ini ${DATADIR}/config/fcadefbneo/fcadefbneo.ini
@@ -35,6 +36,9 @@ cp -n /app/fightcade/Fightcade/emulator/snes9x/fcadesnes9x.default.conf ${DATADI
 echo "N" > ${DATADIR}/config/snes9x/Valid.Ext
 touch ${DATADIR}/config/snes9x/stdout.txt
 touch ${DATADIR}/config/snes9x/stderr.txt
+# Flycast
+mkdir -p ${DATADIR}/config/flycast
+touch ${DATADIR}/config/flycast/emu.cfg
 
 # Boot Fightcade frontend
 /app/bin/zypak-wrapper /app/fightcade/Fightcade/fc2-electron/fc2-electron
