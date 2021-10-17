b5980d2f2d853fed8ce6980cebde7849aa8b37a2
Michael Pobega <pobega@gmail.com>
2021-10-17 14:05:54 -0400

    wine.sh: raise a zenity error window when wine can't be found

---
+++ scripts/wine.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+WINEERROR=$(cat <<-END
+<i>/app/wine/bin/wine</i> is missing.
+
+<b>com.fightcade.Fightcade.Wine</b> is required to run FinalBurn and Snes9x games
+END
+)
+
+WINEPATH="/app/wine/bin/wine"
+export WINEPREFIX=~/.wine64
+
+if [[ -f ${WINEPATH} ]]; then
+	/app/wine/bin/wine "$@"
+else
+	zenity \
+	  --warning \
+	  --title "Flatpak extension required" \
+	  --text "${WINEERROR}"
+fi
