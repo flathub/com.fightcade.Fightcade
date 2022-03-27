fd984e1577f37d1be3d3e05e06724b2d644cabe2
Pobega <Pobega@users.noreply.github.com>
2022-03-27 13:49:51 -0400

    Add a bunch of commenting (#77)

--- scripts/fightcade-launcher.sh
+++ scripts/fightcade-launcher.sh
@@ -5,7 +5,7 @@ DATADIR=/var/data
 # Tell the user that the wine prefix may take some time to create.
 # On first boot it can seem like the application is silently hanging since
 # on slower systems creating the wine prefix can take upwards of 2
-# minutes (tested on a 2-core Silverblue VM)
+# minutes (tested on a 2-core Fedora Silverblue VM)
 echo "Creating or updating wine prefix (/var/data/winepfx), this may take a minute..."
 
 # Silently create/update Wine prefix
@@ -28,19 +28,22 @@ mkdir -p ${DATADIR}/ROMs/flycast
 # Emulator writable files & directories
 # FBNeo
 mkdir -p ${DATADIR}/config/fcadefbneo
-cp -n /app/fightcade/Fightcade/emulator/fbneo/config/fcadefbneo.default.ini ${DATADIR}/config/fcadefbneo/fcadefbneo.ini
+cp -n /app/fightcade/Fightcade/emulator/fbneo/config/fcadefbneo.default.ini ${DATADIR}/config/fcadefbneo/fcadefbneo.ini 2> /dev/null
 # Snes9x
 mkdir -p ${DATADIR}/config/snes9x
 mkdir -p ${DATADIR}/config/snes9x/Saves
-cp -n /app/fightcade/Fightcade/emulator/snes9x/fcadesnes9x.default.conf ${DATADIR}/config/snes9x/fcadesnes9x.conf
-echo "N" > ${DATADIR}/config/snes9x/Valid.Ext
+cp -n /app/fightcade/Fightcade/emulator/snes9x/fcadesnes9x.default.conf ${DATADIR}/config/snes9x/fcadesnes9x.conf 2> /dev/null
 touch ${DATADIR}/config/snes9x/stdout.txt
 touch ${DATADIR}/config/snes9x/stderr.txt
+# Snes9x: Valid.Ext needs a specific value inside or it will cause a crash at runtime.
+echo "N" > ${DATADIR}/config/snes9x/Valid.Ext
 # Flycast
 mkdir -p ${DATADIR}/config/flycast
 touch ${DATADIR}/config/flycast/emu.cfg
 mkdir -p ${DATADIR}/config/flycast/mappings
 mkdir -p ${DATADIR}/config/flycast/data
+# Flycast: copy back in the save states the emulator ships with. If newer ones are available
+# they will be automatically downloaded at runtime.
 cp -R /app/fightcade/Fightcade/emulator/flycast/data_orig/* ${DATADIR}/config/flycast/data
 
 # Boot Fightcade frontend
