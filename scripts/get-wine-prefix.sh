7cbc073c300f5e8af86c2b1488d002352f0975ca
Michael Pobega <pobega@gmail.com>
2023-07-05 10:32:52 -0400

    wine: migrate to new wine prefix subdirectory
    Move to using `/var/data/wineprefixes/` as a subdirectory to contain our
    prefixes. We now use the output of `wine --version` to decide the
    directory name for our prefix (since some users would prefer to install
    an older commit of `com.fightcade.Fightcade.Wine` from Flathub, we need
    to support that user path.)
    
    Also start cleaning out old prefixes from `/var/data` since they're no
    longer needed.

---
+++ scripts/get-wine-prefix.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+PREFIX_DIR=/var/data/wineprefixes
+
+# Create our wineprefix subdirectory if it doesn't exist
+mkdir -p ${PREFIX_DIR}
+
+WINE_VERSION=$(/app/wine/bin/wine --version)
+export WINEPREFIX=${PREFIX_DIR}/${WINE_VERSION}
